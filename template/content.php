6b872df068b121755183402ab629de6d5ae61828
beweb-master <loic.derrieux@gmail.com>
2018-05-08 12:09:45 +0200

    design de la navbar

--- template/content.php
+++ template/content.php
@@ -1,11 +1,15 @@
+<body>
+	<header>
+		<?php include('navbar.php'); ?>
+
+	</header>
 
 <?php
-echo "content";
-echo "<br>";
-include('navbar.php');
+
 //redirection vers la page home quand URI == /
 $_SERVER['REQUEST_URI'] = ($_SERVER['REQUEST_URI'] === "/")?"/home":$_SERVER['REQUEST_URI'];
 
 include("contents". DIRECTORY_SEPARATOR . str_replace("/","",$_SERVER['REQUEST_URI']) . ".php");
 
-echo "<br>";
+?>
+</body>
--- template/navbar.php
+++ template/navbar.php
@@ -5,19 +5,27 @@
 	$aStartTag = "<a href='";
 	
 	$files = scandir($linkPath);
-	?>
+?>
 	
-	<nav>
-		<ul>
-	<?php
-	foreach($files as $file){
-		if(is_file($linkPath . $file)){
-			$f = substr($file,0,strpos($file,".php"));
-			echo "<li><a href='/" . $f . "'>" . str_replace("_"," ",$f) . "</a></li>";
-		}		
-	}
-	
-	?>
+	<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+		<div class="navbar-brand"><i class="fab fa-accessible-icon"></i>Restologist</div>
+		<div class="collapse navbar-collapse" id="navbarCollapse">
+		<ul class="navbar-nav mr-auto">
+<?php
+foreach($files as $file){
+	if(is_file($linkPath . $file)){
+		$f = substr($file,0,strpos($file,".php"));
+		echo "<li class='nav-item'><a class='nav-link' href='/" . $f . "'>" . str_replace("_"," ",$f) . "</a></li>";
+	}		
+}
+
+?>
 		</ul>
+		<form class="form-inline mt-3 mt-md-0">
+			<input class="form-control mr-sm-2" type="text" placeholder="username">
+			<input class="form-control mr-sm-2" type="password" placeholder="password">
+			<button class="btn btn-outline-success my-2 my-sm-0">LOGIN</button>
+		</form>
+		</div>
 	</nav>
 	<?php
