afb4366fe1440f29497d3337612e2c3d1acce87f
beweb-master <loic.derrieux@gmail.com>
2018-05-08 11:22:16 +0200

    update fichier htaccess

--- template/content.php
+++ template/content.php
@@ -3,6 +3,8 @@
 echo "content";
 echo "<br>";
 include('navbar.php');
+//redirection vers la page home quand URI == /
+$_SERVER['REQUEST_URI'] = ($_SERVER['REQUEST_URI'] === "/")?"/home":$_SERVER['REQUEST_URI'];
 
 include("contents". DIRECTORY_SEPARATOR . str_replace("/","",$_SERVER['REQUEST_URI']) . ".php");
 
