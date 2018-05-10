a0fed1e66b76c989a3352dab5bf9509529589ab8
beweb-master <loic.derrieux@gmail.com>
2018-05-10 23:46:26 +0200

    Mise en place de la page d'accueil

--- contents/livre_d_or.php
+++ contents/livre_d_or.php
@@ -5,7 +5,8 @@
 				Vous avez aimé ? Lachez votre commentaire :
 			</h3>		
 			
-	<?php
+	<?php	
+
 			$messages = json_decode(file_get_contents($_SERVER['DOCUMENT_ROOT'] . "/datas/messages.json"),true);
 			if($messages !== null){
 				foreach($messages as $message){
--- template/header.php
+++ template/header.php
@@ -3,6 +3,8 @@
 	<head>
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.12/css/all.css" integrity="sha384-G0fIWCsCzJIMAVNQPfjH08cyYaUtMwjJwqiRKxxE/rx96Uroj1BtIQ6MLJuheaO9" crossorigin="anonymous">
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+		<link rel="stylesheet" href="http://php-decouverte.bwb/css/styles.css">
+	
 	</head>
 
 
